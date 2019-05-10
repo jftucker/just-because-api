3cbbb5dcc037d13b41fed7415b156d81d82acdee
jftucker <johntucker0686@gmail.com>
2019-05-10 13:08:33 -0700

    updated permissions

--- just_because/settings.py
+++ just_because/settings.py
@@ -130,6 +130,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
--- just_because/urls.py
+++ just_because/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/jobs', include('jobs.urls'))
+    path('api/v1/jobs', include('jobs.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
