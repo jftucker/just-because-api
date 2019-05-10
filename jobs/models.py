17121e5ccfb49b5db5c7297910d098738369f56b
jftucker <johntucker0686@gmail.com>
2019-05-09 18:29:12 -0700

    Updating jobs model

--- jobs/models.py
+++ jobs/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class Job(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='author')
+    workers = models.ManyToManyField(User, related_name='workers')
 
     def __str__(self):
         return self.title
