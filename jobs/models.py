d932b487b663e5f674921314a11290d7c48e62d5
jftucker <johntucker0686@gmail.com>
2019-05-14 21:31:04 -0700

    adding fields to jobs model

--- jobs/models.py
+++ jobs/models.py
@@ -12,15 +12,19 @@ class Job(models.Model):
     description = models.TextField()
     size = models.IntegerField(null=True, blank=True)
     cost = models.FloatField(null=True, blank=True)
+    startDate = models.DateField(null=True, blank=True)
+    endDate = models.DateField(null=True, blank=True)
+    points = models.IntegerField(default=0)
+    image = models.ImageField(null=True, blank=True)
+    isFinished = models.BooleanField(default=False)
     location = models.CharField(max_length=50, null=True, blank=True)
     liked = models.ManyToManyField(User, related_name='liked')
     saved = models.ManyToManyField(User, related_name='saved')
     joined = models.ManyToManyField(User, related_name='joined')
-    isFinished = models.BooleanField(default=False)
     contributors = models.ManyToManyField(Contributor, related_name='contributors')
     rewards = models.ManyToManyField(Reward, related_name='rewards')
-    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='author')
     workers = models.ManyToManyField(User, related_name='workers')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='author')
 
     def __str__(self):
         return self.title
